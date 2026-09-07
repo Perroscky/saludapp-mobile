a3032447b35e2d438c92bf53f6e47e5b081bba87
Perroscky <la.samaniegos@uea.edu.ec>
2026-09-07 00:37:09 -0500

    Actualizacion 5

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   // 🔥 PARA EMULADOR ANDROID
-  static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'http://192.168.100.246:8080';
   
   static const String register = '$baseUrl/auth/register';
   static const String login = '$baseUrl/auth/login';
